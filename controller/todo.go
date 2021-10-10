9a7929fd3da8ccd67b3a6c8e214d80b47797575d
erico0301 <76524313+erico0301@users.noreply.github.com>
2021-10-10 17:03:37 +0700

    Update Rest API for todo App

--- controller/todo.go
+++ controller/todo.go
@@ -7,10 +7,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func GetAllTodos(w http.ResponseWriter, r *http.Request) {
 	//Connect DB
+	w.Header().Set("Content-Type", "application/json")
 	db, err := database.Connect()
 	if err != nil {
 		fmt.Println(err.Error())
@@ -79,13 +82,100 @@ func CreateTodo(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetTodo(w http.ResponseWriter, r *http.Request) {
+	// Database Connection
+	w.Header().Set("Content-Type", "application/json")
+	db, err := database.Connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
 
+	//Read HTTP Parameter
+	params := mux.Vars(r)
+	paramId := params["id"]
+
+	//Exec Query Real All Todo
+	rows, err := db.Query("select * from todo where id = ?", paramId)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	//Merubah Hasil rews Query ke struct
+	var result []model.Todo
+	for rows.Next() {
+		var temp = model.Todo{}
+		err := rows.Scan(&temp.Id, &temp.Name, &temp.Complete)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		result = append(result, temp)
+	}
+
+	//Merubah struct ke json untuk di tampilkan
+	res, _ := json.Marshal(result)
+	w.Write(res)
 }
 
 func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	// Database Connection
+	w.Header().Set("Content-Type", "application/json")
+	db, err := database.Connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	//Read HTTP Parameter
+	params := mux.Vars(r)
+	paramId := params["id"]
+
+	//Read Req Body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	var todo model.Todo
+	err = json.Unmarshal(body, &todo)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	_, err = db.Exec("update todo set name = ?, complete = ? where id = ?", todo.Name, todo.Complete, paramId)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	w.Write([]byte("Update Data Succrsfull..."))
 
 }
 
 func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	// Database Connection
+	w.Header().Set("Content-Type", "application/json")
+	db, err := database.Connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
 
+	//Read HTTP Parameter
+	params := mux.Vars(r)
+	paramId := params["id"]
+
+	_, err = db.Exec("delete from todo where id =?", paramId)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte("Delete Data Succrsfull..."))
 }
